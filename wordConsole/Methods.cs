75f6c5c8a492a3c3c5aa7313a3f27cb00633f4e9
David Alcalde <davidalc2012@gmail.com>
2017-01-16 00:48:39 -0600

    Add methods class

---
+++ wordConsole/Methods.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace wordConsole
+{
+    class Methods
+    {
+
+        Word.Application application;
+        Word.Document doc;
+
+        public String openWord()
+        {
+            try
+            {
+                application = new Microsoft.Office.Interop.Word.Application();
+                doc = new Microsoft.Office.Interop.Word.Document();
+            }
+            catch(Exception e)
+            {
+                return e.Message;
+            }
+
+            object missing = System.Reflection.Missing.Value;
+            object readOnly = false;
+            object isVisible = true;
+            object fileName = @"C:\Users\David\Desktop\WpfApplication1\WpfApplication1\Título.docx";
+
+            //object fileName = "http://www.corteidh.or.cr/docs/casos/articulos/seriec_330_esp.docx";
+
+            try
+            {
+                application.Visible = true;
+                doc = application.Documents.Open(ref fileName, ref missing, ref readOnly, ref missing, ref missing, ref missing, 
+                                                ref missing, ref missing, ref missing, ref missing, ref missing, ref isVisible, 
+                                                ref missing, ref missing, ref missing, ref missing);
+                doc.Activate();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            return "WORD DOCUMENT OPEN";
+        }
+
+        public bool findNextInDoc(String toFind)
+        {
+            try
+            {
+                Object text = toFind;
+                application.Selection.Find.ClearFormatting();
+                object missing = System.Reflection.Missing.Value;
+
+                if (application.Selection.Find.Execute(ref text,
+                    ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return false;
+            }
+
+        }
+
+        public int countTextTimes(String toFind)
+        {
+            object what = Word.WdGoToItem.wdGoToLine;
+            object wich = Word.WdGoToDirection.wdGoToAbsolute;
+            object missing = 1;
+            application.Selection.GoTo(what, wich, missing).Select();
+            
+            int count=0;
+            while (findNextInDoc(toFind))
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public String quitWord()
+        {
+            Object saveChanges = Word.WdSaveOptions.wdDoNotSaveChanges;
+            Object originalFormat = Word.WdOriginalFormat.wdOriginalDocumentFormat;
+            Object routeDocument = false;
+            String error="";
+            try
+            {
+                doc.Close(saveChanges, originalFormat, routeDocument);
+            }
+            catch (Exception e)
+            {
+                error= e.Message;
+            }
+            try
+            {
+                application.Quit(saveChanges, originalFormat, routeDocument);
+            }
+            catch (Exception e)
+            {
+                return error+" "+e.Message;
+            }
+            if (error!= ""){
+                return error;
+            }
+            return "Word closed";
+        }
+
+        public void tests()
+        {
+            object start= doc
+            Word.Range rng= doc.
+            application.Selection.SetRange(, 15);
+        }
+        
+    }
+}
+
+/*object what = Word.WdGoToItem.wdGoToLine;
+            object wich = Word.WdGoToDirection.wdGoToAbsolute;
+            object missing = 15;
+            application.Selection.GoTo(what, wich, missing).Select();
+            //System.Console.WriteLine(application.Selection.Range.Information[Word.WdInformation.wdFirstCharacterLineNumber]);
+
+            object unit = Word.WdUnits.wdLine;
+            object extend = Word.WdMovementType.wdExtend;
+            application.Selection.MoveDown(unit, 2, extend);
+
+            //application.Selection.SetRange(0, 15);
+            Word.Characters wchar = application.Selection.Characters;
+            System.Console.WriteLine(wchar.ToString());
+            System.Console.WriteLine(application.Selection.Text);
+            System.Console.ReadLine();
+
+    */
---
+++ wordConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace wordConsole
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            Console.WriteLine(methods.openWord());
+            bool cicle = true;
+            String res;
+            while (cicle)
+            {
+                Console.WriteLine("S=search, C=count, Q=quit");
+                res = Console.ReadLine();
+                switch (res)
+                {
+                    case "q":
+                        Console.WriteLine(methods.quitWord());
+                        cicle = false;
+                        break;
+                    case "s":
+                        res = Console.ReadLine();
+                        Console.WriteLine(methods.findNextInDoc(res));
+                        break;
+                    case "c":
+                        res = Console.ReadLine();
+                        Console.WriteLine(methods.countTextTimes(res));
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
